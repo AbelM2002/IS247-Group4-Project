921bf07f82c91f684af298bbdeca13ae2f6c820e
ConfusedScream <ashwinsingham@gmail.com>
2025-06-23 18:23:10 -0400

    Update Main.java
    Added Notification type choosing

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,36 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Objects;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        System.out.println("Choose notification type:");
+        System.out.println("Email: e");
+        System.out.println("SMS: s");
+        System.out.println("WhatsApp: w");
+
+        Scanner input = new Scanner(System.in);
+        String notif = input.nextLine();
+        
+        int w = 0;
+        while (w == 0){
+            if((Objects.equals(notif, "e")) || (notif.equals("s")) || (notif.equals("w"))){
+                w++;
+            }else{
+                System.out.println("Invalid Input");
+                System.out.println("Choose notification type:");
+                System.out.println("Email");
+                System.out.println("SMS");
+                System.out.println("WhatsApp");
+                notif = input.nextLine();
+            }
+        }
+        // notif = chosen notification type
+
+
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
