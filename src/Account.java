25316db1e3c239810b2984a73bc4c67b7e2071ff
ConfusedScream <ashwinsingham@gmail.com>
2025-07-02 20:36:00 -0400

    Update Account.java
    fixes

--- src/Account.java
+++ src/Account.java
@@ -4,15 +4,18 @@ class Account{
     private int balance;
     private String notification;
     private String contact;
+    //notification and contact are fluff, were used for API
 
-    public Account(int balance, String notification String contact) {
+    public Account(int balance, String notification, String contact) {
         this.balance = balance;
         this.notification = notification;
         this.contact = contact;
     }
 
     public void balance() {
+
         notify("balance is: " + balance,notification,contact);
+
     }
 
     public void deposit(int x){
@@ -20,17 +23,18 @@ class Account{
         notify(x + " dollars have been deposited" + balance,notification,contact);
     }
 
-    public void withdraw(int x){
+    public void withdraw(int x) {
         int temp = balance + x;
-        if(temp < 0){
+        if (temp < 0) {
             notify("overdrawn" + balance,notification,contact);
-        }else{
+
+        } else {
             balance = balance - x;
             notify(x + " dollars have been withdrawn" + balance,notification,contact);
         }
     }
-    
-     private void notify(String s, String notification,String contact) {
+
+    private void notify(String s, String notification,String contact) {
         if(Objects.equals(notification, "e")){
             System.out.println("email to " + contact +  ": " + s +"\n");
         } else if (Objects.equals(notification, "s")) {
@@ -38,5 +42,5 @@ class Account{
         } else if (Objects.equals(notification, "w")) {
             System.out.println("whatsapp message to" + contact + ": : " + s +"\n");
         }
-    }   
+    }
 }
