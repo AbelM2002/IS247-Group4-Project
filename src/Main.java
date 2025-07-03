603d9df75eaded4ea11fe44a1dfb138a4cf3f6ab
ConfusedScream <ashwinsingham@gmail.com>
2025-07-02 20:36:28 -0400

    Update Main.java
    fixes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Objects;
@@ -6,8 +7,6 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-
-        //nofication choosing
         System.out.println("Hello and welcome!");
         System.out.println("Choose notification type:");
         System.out.println("Email: e");
@@ -16,7 +15,7 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
         String notif = input.nextLine();
-        
+
         int w = 0;
         while (w == 0){
             if((Objects.equals(notif, "e")) || (notif.equals("s")) || (notif.equals("w"))){
@@ -24,19 +23,17 @@ public class Main {
             }else{
                 System.out.println("Invalid Input");
                 System.out.println("Choose notification type:");
-                System.out.println("Email");
-                System.out.println("SMS");
-                System.out.println("WhatsApp");
+                System.out.println("Email: e");
+                System.out.println("SMS: s");
+                System.out.println("WhatsApp: w");
                 notif = input.nextLine();
             }
         }
-        // notif = chosen notification type
 
         System.out.println("Enter Contact");
         String contact = input.nextLine();
-        // contact = contact point
 
-        //intial deposti
+
         System.out.println("Enter initial deposit amount");
         int deposit = input.nextInt();
         w = 0;
@@ -46,51 +43,46 @@ public class Main {
             }else{
                 System.out.println("Invalid Input");
                 System.out.println("Enter initial deposit amount");
-                account = input.nextInt();
+                deposit = input.nextInt();
             }
         }
-        //deposit = inital deposit
 
-        System.out.println("\n creating account...");
+        System.out.println("\ncreating account...");
         Account account1 = new Account(deposit,notif,contact);
-        System.out.println("account created \n");
+        System.out.println("account created\n");
 
         int x = 0;
         int amount;
+
         while(x == 0) {
             System.out.println("What would you like to do?");
             System.out.println("View Balance ... B");
             System.out.println("Deposit ... D");
             System.out.println("Withdraw ... W");
             System.out.println("Quit ... Q");
-            String in = input.nextLine();
-            
-            while(Objects.equals(in, "")){
+            String inputs2 = input.nextLine();
+
+            while(Objects.equals(inputs2, "")){
                 inputs2 = input.nextLine();
             }
 
-            if (Objects.equals(in, "Q")){
-                x++;
+            if (Objects.equals(inputs2, "Q")){
+                ++x;
                 System.out.println("Quiting");
-            } else if (Objects.equals(in, "B")) {
+                break;
+            } else if (Objects.equals(inputs2, "B")) {
                 account1.balance();
-            }else if (Objects.equals(in, "D")) {
+            }else if (Objects.equals(inputs2, "D")) {
                 System.out.println("Enter deposit amount:");
                 amount = input.nextInt();
                 account1.deposit(amount);
-            }else if (Objects.equals(in, "W")) {
+            }else if (Objects.equals(inputs2, "W")) {
+                System.out.println("Enter withdraw amount:");
                 amount = input.nextInt();
                 account1.withdraw(amount);
-            }else{
+            } else{
                 System.out.println("Invalid Input");
             }
-        }
-
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
         }
     }
 }
